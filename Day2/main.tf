09ef7a8b35cd04f40dc0a71bb8d63bc7bc0940fc
Badhon58 <badhon@theclinicall.com>
2026-09-15 11:09:53 +0600

    Error Remove

--- Day2/main.tf
+++ Day2/main.tf
@@ -1,6 +1,6 @@
 # create s3 bucket
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "badhon_new_bucket_12345"
+  bucket = "badhon-new-bucket-12345"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
